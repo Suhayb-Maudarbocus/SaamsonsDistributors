a396a46e9402ef2bae7d9e21b6e9b6ab26c6d416
maudar <s.maudar6@gmail.com>
2025-08-10 13:32:33 +0400

    Added cart model BE + Migrations

--- SAAMSONSDISTRIBUTORS/Models/AppDbContext.cs
+++ SAAMSONSDISTRIBUTORS/Models/AppDbContext.cs
@@ -7,5 +7,6 @@ namespace SAAMSONSDISTRIBUTORS.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
     }
 }
---
+++ SAAMSONSDISTRIBUTORS/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAAMSONSDISTRIBUTORS.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product? Product { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+    }
+}
